86daf5f939e01dcb62adcd9ed609326c538a8ecc
Максим <gmax7911@gmail.com>
2023-01-29 12:42:02 +0300

    q

--- src/Cat.java
+++ src/Cat.java
@@ -1,16 +1,64 @@
+import java.time.LocalDate;
+import java.util.Arrays;
+
 public class Cat {
 
     String name;
-    int age;
+   private int birthYear;
 
-    public Cat() {
-        name = "КОт";
-        age = 1;
+    private Cat[] friends;
+
+    public Cat(String name) {
+        this(name, LocalDate.now().getYear());
+        this.name = name;
+        birthYear = 1;
+    }
+
+    public Cat(String name, int age) {
+        this.name = name;
+
+        if (age >= 0) {
+            this.birthYear = LocalDate.now().getYear() - age;
+        } else {
+            this.birthYear = LocalDate.now().getYear() - Math.abs(age);
+        }
+ //       friends = new Cat[10];
     }
 
     void meow() {
+        System.out.println();
         System.out.println("Функция мяу");
-        System.out.println("Меня зовут " + name);
-        System.out.println("Мне " + age + " лет");
+        System.out.println("Меня зовут " + getName());
+        System.out.println("Мне " + getage() + " лет");
+        System.out.println();
+    }
+
+
+    Cat[] getFriends(){
+        if (friends == null) {
+            friends = new Cat[0];
+        }
+        return friends;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name != null && !name.isEmpty() && !name.isBlank()) {
+            this.name = name;
+        }
+    }
+
+    public int getage() {
+        return LocalDate.now().getYear() - birthYear;
+    }
+
+    void addFriend(Cat friend) {
+        this.friends = Arrays.copyOf(this.friends, getFriends().length + 1);
+        this.friends[friends.length - 1] = friend;
+
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        Cat murzik = new Cat();
+        Cat murzik = new Cat("Мурзик", 10);
 
       //  murzik.name = "Мурзик";
-      //  murzik.age = 3;
+
 
         murzik.meow();
 
-        Cat begemot = new Cat();
-       // begemot.name = "Бегемот";
-       // begemot.age = 1;
+        Cat begemot = new Cat("Бегемот", -5);
+
+        murzik.setName(null);
         begemot.meow();
+
+//        murzik.friends = null;
+//
+//
+        System.out.println("У кота " + murzik.name + " " + murzik.getFriends().length +  " друзей");
+
+//        murzik.addFriend(begemot);
+
+        System.out.println("У кота " + murzik.name + " " + murzik.getFriends().length +  " друзей");
+//
+//        murzik.friends[0].meow();
+
+
+
     }
 }
